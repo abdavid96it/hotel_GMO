9ece08edbab957997162408c1a6e1b7b8064ceac
at-oanh.le <kimoanhle1996@gmail.com>
2018-05-14 16:54:37 +0700

    function login

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,13 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+
+User.create!( name: "oanhle",
+     email: "kimoanhle1996@gmail.com",
+     password: "123456",
+     role: true,
+     # active_digest: true,
+     #  activated: true,
+     #  activated_at: Time.zone.now
+     )
